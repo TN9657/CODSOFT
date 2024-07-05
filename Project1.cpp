8253547a64d078e5229418a5297e8fe30ad4298d
TN9657 <tanmaynirgude123@gmail.com>
2024-07-05 09:44:44 +0530

    feature signup working

--- Project1.cpp
+++ Project1.cpp
@@ -1,3 +1,8 @@
+/* Create a program that generates a random number and asks the
+user to guess it. Provide feedback on whether the guess is too
+high or too low until the user guesses the correct number
+*/
+
 #include<iostream>
 #include<random>
 using namespace std;
